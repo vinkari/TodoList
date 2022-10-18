ee2716f0138b9f936f7b6da7e74ea5cfb858d46b
Vinod Karivelil <vinodkarivelil@gmail.com>
2022-10-18 14:55:38 -0300

    updated todolist

--- todolist.js
+++ todolist.js
@@ -4,7 +4,7 @@ class Todo {
 
   constructor(title) {
     this.title = title;
-    this.done = done;
+    this.done = false;
   }
 
   toString() {
@@ -26,5 +26,83 @@ class Todo {
 
   getTitle() {
     return this.title;
+  }
+}
+
+class TodoList {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  add(todo) {
+    if (!(todo instanceof Todo)) {
+      throw new TypeError('can only add Todo objects');
+    }
+    this.todos.push(todo);
+  }
+
+  size() {
+    return this.todos.length;
+  }
+
+  first() {
+    return this.todos[0];
+  }
+
+  last() {
+    return this.todos[this.size() - 1];
+  }
+
+  _validateIndex(index) {
+    let validIndexes = this.todos.map((_, index) => index);
+
+    if (!validIndexes.includes(index)) {
+      throw new ReferenceError(`invalid index: ${index}`);
+    }
+  }
+
+  itemAt(index) {
+    this._validateIndex(index);
+    return this.todos[index];
+  }
+
+  markDoneAt(index) {
+    this.itemAt(index).markDone();
+  }
+
+  markUndoneAt(index) {
+    this.itemAt(index).markUndone();
+  }
+
+  isDone() {
+    for (let index = 0; index < this.todos.length; index++) {
+      if (!this.todos[index].isDone()) return false;
+    }
+    return true;
+  }
+
+  shift() {
+    return this.todos.shift();
+  }
+
+  pop() {
+    return this.todos.pop();
+  }
+
+  removeAt(index) {
+    this._validateIndex(index);
+    return this.todos.splice(index, 1)[0];
+  }
+
+  toString() {
+    let title = `--- ${this.title} ---\n`;
+    let todoItems = '';
+
+    this.todos.forEach((todo, index) => {
+      todoItems += todo.toString() + (index === this.size() - 1 ? '' : '\n');
+    })
+
+    return title + todoItems;
   }
 }
